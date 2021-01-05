fe123dcf8b525e991f35d8595dffbaba9aebcf14
Alejandro Perez Gancedo <jandrito97@gmail.com>
2021-01-05 23:52:31 +0100

    Added new css rules and display the array of blogs

--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,21 @@ import { useState } from 'react'
 const Home = () => {
     //let name = 'Me'
     const [blogs, setBlogs] = useState([
-        {title:'Test', body: 'lorem ipsum', author:'test author', id:1}
+        {title:'Test', body: 'lorem ipsum', author:'test author', id:1},
+        {title:'Test', body: 'lorem ipsum', author:'test author', id:2}
     ]);
-    const [name,setName]=useState('Me');
-    const [age,setAge]=useState(25);
-    const handleClick = (e) => {
-        setName('you')
-        setAge(30)
-        console.log( name, age)
-    }
-    const handleClickWithParameters = (name,e) => {
-        console.log('Hello world'+ name, e.target)
-    }
-    
 
-    return ( <div className="home">
-        <h2>Home Page</h2>
-        <p>{name} is {age} old</p>
-        <button onClick={handleClick}>Click Me</button>
-        <button onClick={(e)=>handleClickWithParameters('Test Name',e)}>Click Me Parameters</button>
-    </div> 
+
+    return ( 
+        <div className="home">
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+
+        </div> 
     );
 }
  
