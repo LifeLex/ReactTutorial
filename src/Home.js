6074103224d985509c9f950a7d7427dee95d5ba1
Alejandro Perez Gancedo <jandrito97@gmail.com>
2021-01-06 19:07:00 +0100

    Added delete button with its function

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,20 @@ const Home = () => {
     //let name = 'Me'
     const [blogs, setBlogs] = useState([
         {title:'Test', body: 'lorem ipsum', author:'test author', id:1},
-        {title:'Test2', body: 'lorem ipsum', author:'test author', id:2}
-    ]);
+        {title:'Test2', body: 'lorem ipsum', author:'test author2', id:2},
+        {title:'Test3', body: 'lorem ipsum', author:'test author3', id:3},
 
+    ]);
+    const handleDelete = (id) => {
+       const newBlogs = blogs.filter((blog)=>blog.id !== id)
+       setBlogs( newBlogs );    
+       
+    }
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs}/>
+            <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog)=>blog.author==='test author')} title='Test Blogs' handleDelete={handleDelete}/>
         </div> 
     );
 }
