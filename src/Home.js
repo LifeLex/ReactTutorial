e8b0f2e6af2b0b3b08c7e9fac0a7f865782b627a
Alejandro Perez Gancedo <jandrito97@gmail.com>
2021-01-08 19:51:16 +0100

    Added fetch from json-server using useEffectHook

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,30 @@ import BlogList from './BlogList'
 import { useState, useEffect } from 'react'
 
 const Home = () => {
-    const [name, setName] = useState('mario')
-    const [blogs, setBlogs] = useState([
-        {title:'Test', body: 'lorem ipsum', author:'test author', id:1},
-        {title:'Test2', body: 'lorem ipsum', author:'test author2', id:2},
-        {title:'Test3', body: 'lorem ipsum', author:'test author3', id:3},
-
-    ]);
+    //const [name, setName] = useState('mario')
+    const [blogs, setBlogs] = useState(null);
     const handleDelete = (id) => {
        const newBlogs = blogs.filter((blog)=>blog.id !== id)
        setBlogs( newBlogs );    
        
     }
     useEffect(() => {
-      console.log('use effect run')
-      console.log(name)
-    }, [name])
+      fetch('http://localhost:8000/blogs')
+        .then(res=>{
+           return res.json();
+        })
+        .then((data)=>{
+            setBlogs(data)
+        })
+    }, [])
     
    
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog)=>blog.author==='test author')} title='Test Blogs' handleDelete={handleDelete}/>
-            <button onClick={()=> setName('luigi')}>Change name</button>
-            <p>{name}</p>
+            {blogs && <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>}
+            {/* <BlogList blogs={blogs.filter((blog)=>blog.author==='test author')} title='Test Blogs' handleDelete={handleDelete}/> */}
+            {/* <button onClick={()=> setName('luigi')}>Change name</button> */}
+            {/* <p>{name}</p> */}
         </div> 
     );
 }
