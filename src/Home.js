08f6c31ca95367082e57b4fcec85c8b513cabad0
Alejandro Perez Gancedo <jandrito97@gmail.com>
2021-01-08 16:58:38 +0100

    Use Effect

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 import BlogList from './BlogList'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
-    //let name = 'Me'
+    const [name, setName] = useState('mario')
     const [blogs, setBlogs] = useState([
         {title:'Test', body: 'lorem ipsum', author:'test author', id:1},
         {title:'Test2', body: 'lorem ipsum', author:'test author2', id:2},
@@ -14,11 +14,18 @@ const Home = () => {
        setBlogs( newBlogs );    
        
     }
-
+    useEffect(() => {
+      console.log('use effect run')
+      console.log(name)
+    }, [name])
+    
+   
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog)=>blog.author==='test author')} title='Test Blogs' handleDelete={handleDelete}/>
+            <button onClick={()=> setName('luigi')}>Change name</button>
+            <p>{name}</p>
         </div> 
     );
 }
