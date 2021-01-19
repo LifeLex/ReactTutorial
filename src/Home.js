22b221019cfb3ca25584c8565226f782f523e166
Alejandro Perez Gancedo <jandrito97@gmail.com>
2021-01-19 22:23:50 +0100

    handling errors for fetching data

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,41 @@ import { useState, useEffect } from 'react'
 const Home = () => {
     //const [name, setName] = useState('mario')
     const [blogs, setBlogs] = useState(null);
+    const[isPending, setIsPending]= useState(true);
+    const[error, setError]= useState(null);
     const handleDelete = (id) => {
        const newBlogs = blogs.filter((blog)=>blog.id !== id)
        setBlogs( newBlogs );    
        
     }
     useEffect(() => {
-      fetch('http://localhost:8000/blogs')
+      setTimeout(()=>{
+        fetch('http://localhost:8000/blogs')
         .then(res=>{
+            if(!res.ok){
+              throw Error("Could not fetch the date from that resource")      
+            }
            return res.json();
         })
         .then((data)=>{
             setBlogs(data)
+            setIsPending(false)
+            setError(null);
         })
-    }, [])
+        .catch((err)=>{
+            setError(err.message);
+            setIsPending(false);
+            console.log(err.message)
+        })
+      }, 1000)
+    }, []);
     
    
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading</div>}
+            {/* Logical and */}
             {blogs && <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete}/>}
             {/* <BlogList blogs={blogs.filter((blog)=>blog.author==='test author')} title='Test Blogs' handleDelete={handleDelete}/> */}
             {/* <button onClick={()=> setName('luigi')}>Change name</button> */}
